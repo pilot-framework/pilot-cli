9b8b7d1cb4c22cef8ae9e922806f263179e09986
Carl <jerritt.aquino@gmail.com>
2021-07-21 16:04:30 -0400

    modify console log calls to use this

--- src/util/init.ts
+++ src/util/init.ts
@@ -9,10 +9,10 @@ const makeDir = (path: string) => {
   }
 }
 
-export const initialize = async () => {
+export async function initialize() {
   // Create ~/.pilot file structure
   if (fs.existsSync(paths.PILOT_CONFIG)) {
-    console.log('Pilot configuration detected...')
+    this.log('Pilot configuration detected...')
   } else {
     makeDir(paths.PILOT_CONFIG)
     makeDir(paths.PILOT_AWS)
@@ -25,29 +25,29 @@ export const initialize = async () => {
   /***********************/
   // if ~/.aws directory exists, copy contents
   if (fs.existsSync(paths.AWS_CONFIG) && !fs.existsSync(paths.PILOT_AWS_CONFIG)) {
-    console.log('AWS configuration detected...copying...')
+    this.log('AWS configuration detected...copying...')
     fs.copyFile(paths.AWS_CONFIG, paths.PILOT_AWS_CONFIG, err => {
       if (err) {
-        console.log('ERROR: ', err)
+        this.log('ERROR: ', err)
       } else {
-        console.log(`AWS config copy success!`)
+        this.log(`AWS config copy success!`)
       }
     })
   } else {
-    console.log('No AWS CLI configuration detected')
+    this.log('No AWS CLI configuration detected')
   }
 
   if (fs.existsSync(paths.AWS_CREDENTIALS)) {
-    console.log('AWS credentials detected...copying...')
+    this.log('AWS credentials detected...copying...')
     fs.copyFile(paths.AWS_CREDENTIALS, paths.PILOT_AWS_CREDENTIALS, err => {
       if (err) {
-        console.log('ERROR: ', err)
+        this.log('ERROR: ', err)
       } else {
-        console.log(`AWS credentials copy success!`)
+        this.log(`AWS credentials copy success!`)
       }
     })
   } else {
-    console.error('ERROR: Please configure AWS CLI with access keys. Run \'aws configure\'')
+    this.error('ERROR: Please configure AWS CLI with access keys. Run \'aws configure\'')
   }
 
   /***********************/
