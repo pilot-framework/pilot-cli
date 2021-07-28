81545418e5a7350ea0fbe2dbe07f6333ae52b8d9
Vincent Bundage <vincentbundage@gmail.com>
2021-07-28 15:45:31 -0600

    feat: select project during new app

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1,12 +1,6 @@
 import {Command, flags} from '@oclif/command'
 import * as inquirer from 'inquirer'
-import cli from 'cli-ux'
-import paths from '../util/paths'
-import fsUtil from '../util/fs'
-
-const fs = require('fs')
-
-const CWD = process.cwd()
+import waypoint from '../util/waypoint'
 
 export default class New extends Command {
   static description = 'Initializes a project or application to be used with Pilot'
@@ -22,7 +16,7 @@ export default class New extends Command {
       required: true,
       description: 'the type you are trying to initialize (project or app)',
       options: ['project', 'app'],
-    }
+    },
   ]
 
   async run() {
@@ -30,49 +24,52 @@ export default class New extends Command {
       flags,
       args,
     } = this.parse(New)
-    
+
     if (!flags.bare) {
-      this.log(`Initializing a new ${args.type} will scaffold a new directory.
-If you want to configure an existing project, re-run this with the '--bare' flag in the top-level directory.`)
+    //       this.log(`Initializing a new ${args.type} will scaffold a new directory.
+    // If you want to configure an existing project, re-run this with the '--bare' flag in the top-level directory.`)
     }
-    
-    let projectName = ''
-    let provider = ''
 
     if (args.type === 'project') {
+      this.log(`Initializing a new ${args.type} will scaffold a new directory.`)
       const responses = await inquirer.prompt([
         {
           name: 'name',
           message: 'Project name:',
           type: 'input',
         },
+      ])
+
+      this.log(`Created project: ${responses.name}`)
+      this.log('A project needs at least one application. You can initialize an application by running \'pilot new app\'')
+    }
+
+    if (args.type === 'app') {
+      const appChoices = await inquirer.prompt([
+        {
+          name: 'name',
+          message: 'Application name:',
+          type: 'input',
+        },
+        {
+          name: 'project',
+          message: 'Select a project:',
+          type: 'list',
+          choices: (await waypoint.getProjects()),
+        },
         {
           name: 'provider',
           message: 'Select a cloud provider:',
           type: 'list',
-          choices:[{name: 'AWS'}, {name: 'GCP'}],
+          choices: ['AWS', 'GCP'],
+        },
+        {
+          name: 'stack',
+          message: 'Is this part of the project frontend-only (i.e. compiles into static files for web hosting)?',
+          type: 'confirm',
         },
       ])
-
-      projectName = responses.name
-      provider = responses.provider
-
-      this.log('A project needs at least one application. You can go through the following steps again by running \'pilot new app\'')
+      this.log(appChoices)
     }
-
-    const appChoices = await inquirer.prompt([
-      {
-        name: 'name',
-        message: 'Application name:',
-        type: 'input',
-      },
-      {
-        name: 'stack',
-        message: 'Is this part of the project frontend-only (i.e. compiles into static files for web hosting)?',
-        type: 'confirm',
-      }
-    ])
-
-    this.log(`You're initializing ${projectName} for ${provider} with the application ${appChoices.name} which ${appChoices.stack ? 'frontend-only' : 'fullstack'}.`)
   }
 }
