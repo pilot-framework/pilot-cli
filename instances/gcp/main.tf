3ce7c880c2c625405577d1a89e8cd2d12f87d165
grahamhub <grammar@hey.com>
2021-07-28 12:02:07 -0500

    tf/gcp: use cloud init

--- instances/gcp/main.tf
+++ instances/gcp/main.tf
@@ -79,27 +79,7 @@ resource "google_compute_instance" "pilot-instance" {
   }
 
   metadata = {
-<<<<<<< HEAD
     user-data = file("../../templates/ssh-docker-waypoint-init.yaml")
-=======
-    startup-script = <<-EOF
-      #!/bin/sh
-      sudo curl -fsSL https://get.docker.com -o get-docker.sh 
-      sudo sh get-docker.sh           
-      sudo apt-get install -y software-properties-common
-      curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-      sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-      sudo apt-get update && sudo apt-get install -y waypoint
-      echo {\\"hosts\\": [\\"tcp://0.0.0.0:2375\\", \\"unix:///var/run/docker.sock\\"]} | sudo tee /etc/docker/daemon.json > /dev/null
-      sudo mkdir /etc/systemd/system/docker.service.d
-      echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/override.conf > /dev/null
-      echo "ExecStart=" | sudo tee -a /etc/systemd/system/docker.service.d/override.conf > /dev/null
-      echo "ExecStart=/usr/bin/dockerd" | sudo tee -a /etc/systemd/system/docker.service.d/override.conf > /dev/null
-      # sudo systemctl daemon-reload
-      # sudo systemctl restart docker.service
-      waypoint install -platform=docker -docker-server-image=pilotframework/pilot-waypoint -accept-tos
-    EOF
->>>>>>> 35cd1c73f69731e3fb90df42e9a95ff66429fe9b
   }
 
   service_account {
@@ -128,6 +108,6 @@ resource "google_compute_firewall" "pilot-firewall" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "8080", "1000-2000", "9701", "9702", "2375"]
+    ports    = ["22", "80", "8080", "1000-2000", "9701", "9702", "2375Z"]
   }
 }
