c48d3fe688498dd6c036e628f20ce224d53b66a6
Carl <jerritt.aquino@gmail.com>
2021-07-30 10:11:51 -0400

    refactor gcp setup to use cli.ts

--- src/util/gcp/setup.ts
+++ src/util/gcp/setup.ts
@@ -1,36 +1,24 @@
 import cli from 'cli-ux'
-import chalk from 'chalk'
-const ora = require('ora')
 import paths from '../paths'
 import execUtil from './exec'
 import { existsSync } from 'fs'
 import fs from '../fs'
 import waypoint from '../waypoint'
 import { SetupOpts } from '../../commands/setup'
+import { pilotSpinner, successText, failText } from '../cli'
 
 const timeout = (ms: number): Promise<number> => {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 export async function gcpSetup(opts: SetupOpts) {
-  const spinner = ora()
-  spinner.spinner = {
-    interval: 500,
-    frames: [
-      '\u2708  ',
-      ' \u2708 ',
-      '  \u2708',
-    ],
-  }
-
-  spinner.color = 'gray'
-  const success = chalk.bold.green
+  const spinner = pilotSpinner()
 
   // Check for gcloud config
   // ~/.pilot/gcp/config
   try {
     if (!existsSync(paths.PILOT_GCP_CONFIG)) {
-      console.log(chalk.bold.red('ERROR: no gcloud configuration found. Please run \'pilot init\''))
+      console.log(failText('ERROR: no gcloud configuration found. Please run \'pilot init\''))
       return
     }
 
@@ -56,17 +44,17 @@ export async function gcpSetup(opts: SetupOpts) {
     spinner.text = 'Generating cloud-init configuration file'
     await fs.genCloudInitYaml()
     // console.log('Successfully generated cloud init')
-    spinner.succeed(success('Initial resources configured'))
+    spinner.succeed(successText('Initial resources configured'))
 
     // Configure GCP IAM user / role
     spinner.start('Configuring IAM user and role for Pilot on GCP')
     await execUtil.pilotUserInit(false)
-    spinner.succeed(success('IAM user and role configured'))
+    spinner.succeed(successText('IAM user and role configured'))
 
     // terraform init
     spinner.start('Initializing Terraform')
     await execUtil.terraInit()
-    spinner.succeed(success('Terraform initialized'))
+    spinner.succeed(successText('Terraform initialized'))
 
     // terrform apply --auto-approve
     spinner.start('Provisioning resources. This can take a few minutes...\u2615')
@@ -81,16 +69,16 @@ export async function gcpSetup(opts: SetupOpts) {
     if (!reachable) {
       cli.error('GCE instance initialization timed out')
     }
-    spinner.succeed(success('GCE instance provisioned'))
+    spinner.succeed(successText('GCE instance provisioned'))
 
     spinner.start('Setting docker connection firewall rule...')
     await execUtil.setDockerConnection()
-    spinner.succeed(success('Docker connection firewall rule set'))
+    spinner.succeed(successText('Docker connection firewall rule set'))
 
     // install waypoint post gcloud initialization
     spinner.start('Setting up your remote Waypoint server')
     await execUtil.installWaypoint(opts)
-    spinner.succeed(success('Remote Waypoint server installed'))
+    spinner.succeed(successText('Remote Waypoint server installed'))
 
     spinner.start('Setting context...')
     await execUtil.setContext()
@@ -99,7 +87,7 @@ export async function gcpSetup(opts: SetupOpts) {
     await execUtil.pilotUserInit(true)
     await waypoint.setDefaultContext()
     await execUtil.configureRunner()
-    spinner.succeed(success('Final configurations completed'))
+    spinner.succeed(successText('Final configurations completed'))
   } catch (error) {
     cli.error(error)
   }
