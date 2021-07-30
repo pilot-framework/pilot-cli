418c1a443e2ad402ff875ab7d41957c0ea7189e6
Vincent Bundage <vincentbundage@gmail.com>
2021-07-29 18:15:14 -0600

    feat: added basic hcl templating for frontend

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1,13 +1,21 @@
 import {Command, flags} from '@oclif/command'
 import * as inquirer from 'inquirer'
 import waypoint from '../util/waypoint'
+import tmpl from '../util/templates'
+import fs from '../util/fs'
+import { cwd } from 'process'
+import { join } from 'path'
+import { appendFile } from 'fs'
 
 export default class New extends Command {
   static description = 'Initializes a project or application to be used with Pilot'
 
   static flags = {
     help: flags.help({char: 'h'}),
-    bare: flags.boolean({char: 'b'}),
+    bare: flags.boolean({
+      char: 'b',
+      description: 'Generates Waypoint\'s template project hcl file.',
+    }),
   }
 
   static args = [
@@ -25,13 +33,13 @@ export default class New extends Command {
       args,
     } = this.parse(New)
 
-    if (!flags.bare) {
-    //       this.log(`Initializing a new ${args.type} will scaffold a new directory.
-    // If you want to configure an existing project, re-run this with the '--bare' flag in the top-level directory.`)
-    }
-
     if (args.type === 'project') {
-      this.log(`Initializing a new ${args.type} will scaffold a new directory.`)
+      if (flags.bare) {
+        fs.createFile(join(cwd(), '/waypoint.hcl'), tmpl.standardHCLTemplate())
+        return
+      }
+
+      this.log(`Initializing a new ${args.type} will create a project on the Waypoint server.`)
       const project = await inquirer.prompt([
         {
           name: 'name',
@@ -41,17 +49,13 @@ export default class New extends Command {
       ])
 
       await waypoint.newProject(project.name)
-      this.log(`Created project: ${project.name}`)
-      this.log('A project needs at least one application. You can initialize an application by running \'pilot new app\'')
+      this.log(`\nCreated project: ${project.name}`)
+      this.log('Configure the project from the UI of your Waypoint server.')
+      this.log('A project needs at least one application. You can initialize applications by running \'pilot new app\'.')
     }
 
     if (args.type === 'app') {
-      const appChoices = await inquirer.prompt([
-        {
-          name: 'name',
-          message: 'Application name:',
-          type: 'input',
-        },
+      const initChoices: any | undefined = await inquirer.prompt([
         {
           name: 'project',
           message: 'Select a project:',
@@ -65,12 +69,86 @@ export default class New extends Command {
           choices: ['AWS', 'GCP'],
         },
         {
-          name: 'stack',
-          message: 'Is this part of the project frontend-only (i.e. compiles into static files for web hosting)?',
-          type: 'confirm',
+          name: 'amount',
+          message: 'Number of applications your project contains:',
+          type: 'number',
         },
       ])
-      this.log(appChoices)
+      console.clear()
+
+      interface HCLAttributes {
+        appName: string;
+        frontend: boolean;
+        entryDir: string;
+        bucket: string;
+        region: string;
+        project: string;
+        domain: string;
+      }
+
+      const appChoices: Array<HCLAttributes> = []
+      for (let count = 1; count <= initChoices.amount; count += 1) {
+        // eslint-disable-next-line no-await-in-loop
+        const choices: any | undefined = await inquirer.prompt([
+          {
+            name: 'appName',
+            message: 'Application name:',
+            type: 'input',
+          },
+          {
+            name: 'frontend',
+            message: 'Is this part of the project frontend-only (i.e. compiles into static files for web hosting)?',
+            type: 'confirm',
+          },
+        ])
+          .then(async choices => {
+            const moreChoices = await inquirer.prompt([
+              {
+                name: 'entryDir',
+                message: 'What is the entrypoint directory based on the root of your project?',
+                type: 'input',
+                when: () => choices.frontend === true,
+              },
+              {
+                name: 'bucket',
+                message: 'Name of your bucket:',
+                type: 'input',
+                when: choices.frontend,
+              },
+              {
+                name: 'region',
+                message: 'Region to deploy/release to:',
+                type: 'input',
+              },
+              {
+                name: 'project',
+                message: 'GCP project ID:',
+                type: 'input',
+                when: () => initChoices.provider === 'GCP',
+              },
+              {
+                name: 'domain',
+                message: 'Domain for Cloud CDN release:',
+                type: 'input',
+                when: () => initChoices.provider === 'GCP' && choices.frontend,
+              },
+            ])
+            return Object.assign(choices, moreChoices)
+          })
+        appChoices.push(choices)
+        console.clear()
+      }
+
+      let content = `project = ${initChoices.project}`
+      appChoices.forEach(app => {
+        if (initChoices.provider === 'AWS' && app.frontend) {
+          content += '\n\n' + tmpl.appAWSFrontendHCL(app)
+        }
+        if (initChoices.provider === 'GCP' && app.frontend) {
+          content += '\n\n' + tmpl.appGCPFrontendHCL(app)
+        }
+      })
+      fs.createFile(join(cwd(), '/waypoint.hcl'), content)
     }
   }
 }
--- src/util/templates.ts
+++ src/util/templates.ts
@@ -59,6 +59,122 @@ runcmd:
   - sudo systemctl daemon-reload
   - sudo systemctl restart docker.service`
 
+interface HCLAttributes {
+  appName: string;
+  entryDir: string;
+  region: string;
+  bucket: string;
+  project: string;
+  domain: string;
+}
+
+const appAWSFrontendHCL = (attrs: HCLAttributes) =>
+  `# Name of your application
+app "${attrs.appName}" {
+  build {
+      # The template uses a default build plugin provided by Pilot
+      # to build your static files for deployment
+      use "yarn" {
+          # The application entrypoint in relation to the root of your project/repo
+          # example: directory = "./sub_dir/my_app"
+          directory = "./${attrs.entryDir}"
+      }
+  }
+
+  deploy {
+      # The template uses a default deploy plugin provided by Pilot
+      # to deploy your static assets to a S3 bucket with static web hosting enabled
+      use "pilot-cloudfront" {
+          # Your chosen AWS region
+          region = "${attrs.region}"
+          # This should be a globally unique bucket name
+          bucket = "${attrs.bucket}"
+          # Location of build files in relation to root of project/repo
+          directory = "./${attrs.entryDir}/build/"
+      }
+  }
+
+  release {
+      # The template uses a default release plugin provided by Pilot
+      # to deploy your static site to a Cloudfront distribution
+      use "pilot-cloudfront" {}
+  }
+}`
+
+const appGCPFrontendHCL = (attrs: HCLAttributes) =>
+  `# Name of your application
+app "${attrs.appName}" {
+  build {
+      # The template uses a default build plugin provided by Pilot
+      # to build your static files for deployment
+      use "yarn" {
+          # The application entrypoint in relation to the root of your project/repo
+          # example: directory = "./sub_dir/my_app"
+          directory = "./${attrs.entryDir}"
+      }
+  }
+
+  deploy {
+      # The template uses a default deploy plugin provided by Pilot
+      # to deploy your static assets to a Cloud Storage bucket
+      use "pilot-cloud-cdn" {
+          # Your GCP Project ID - this will be the default Project ID if used with Pilot
+          project = "${attrs.project}"
+          # This should be a globally unique bucket name
+          bucket = "${attrs.bucket}"
+          # Your chosen GCP region
+          region = "${attrs.region}"
+          # Location of build files in relation to root of project/repo
+          directory = "./${attrs.entryDir}/build"
+      }
+  }
+
+  release {
+      # The template uses a default release plugin provided by Pilot
+      # to deploy your static site to a Cloud CDN distribution
+      use "pilot-cloud-cdn" {
+          domain = "${attrs.domain}"
+      }
+  }
+}`
+
+const standardHCLTemplate = () =>
+  `# The name of your project. A project typically maps 1:1 to a VCS repository.
+# This name must be unique for your Waypoint server. If you're running in
+# local mode, this must be unique to your machine.
+project = "my-project"
+
+# Labels can be specified for organizational purposes.
+# labels = { "foo" = "bar" }
+
+# An application to deploy.
+app "web" {
+    # Build specifies how an application should be deployed. In this case,
+    # we'll build using a Dockerfile and keeping it in a local registry.
+    build {
+        use "docker" {}
+
+        # Uncomment below to use a remote docker registry to push your built images.
+        #
+        # registry {
+        #   use "docker" {
+        #     image = "registry.example.com/image"
+        #     tag   = "latest"
+        #   }
+        # }
+
+    }
+
+    # Deploy to Docker
+    deploy {
+        use "docker" {}
+    }
+}
+`
+
 export default {
   yamlConfig,
+  appAWSFrontendHCL,
+  appGCPFrontendHCL,
+  standardHCLTemplate,
 }
