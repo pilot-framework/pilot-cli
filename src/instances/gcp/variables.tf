c888dd8993d600a7c44a79fa1d1e1b0e430a85d9
grahamhub <grammar@hey.com>
2021-08-02 15:00:20 -0500

    cmd/init: rm old templates

--- src/instances/gcp/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "default_project" {
-  description = "The default GCP project configured via gcloud"
-}
-
-variable "default_zone" {
-  description = "The zone where the Pilot server is provisioned"
-}
