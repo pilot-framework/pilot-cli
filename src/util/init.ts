e9e30a77197807af27746bce343fc5c06563b49c
Carl <jerritt.aquino@gmail.com>
2021-08-02 19:44:05 -0400

    update output to use cli.ts

--- src/util/init.ts
+++ src/util/init.ts
@@ -6,12 +6,18 @@ import {
   copyFile,
 } from 'fs'
 import templates from './templates'
+import { pilotSpinner, successText, failText, pilotText  } from './cli'
+import { fail } from 'assert'
 
 export async function initialize(overridePolicyPath: string | null) {
+  const spinner = pilotSpinner()
+  spinner.start()
+
   // Create ~/.pilot file structure
   if (existsSync(paths.CONFIG)) {
-    console.log('Pilot configuration detected...')
+    spinner.text = 'Pilot configuration detected'
   } else {
+    spinner.text = 'Scaffolding directories and files'
     await fsUtil.mkDir(paths.CONFIG)
     await fsUtil.mkDir(paths.PILOT_BIN)
     await fsUtil.mkDir(paths.TEMPLATES)
@@ -36,7 +42,7 @@ export async function initialize(overridePolicyPath: string | null) {
   /*  INSTALL BINARIES  */
   /**********************/
   if (!existsSync(paths.TERRAFORM_EXEC) && !existsSync(paths.WAYPOINT_EXEC)) {
-    console.log('Installing binaries...')
+    spinner.text = 'Installing binaries'
     await fsUtil.installBinaries()
   }
 
@@ -45,58 +51,50 @@ export async function initialize(overridePolicyPath: string | null) {
   /***********************/
   // if ~/.aws directory exists, copy contents
   if (existsSync(paths.AWS_CONFIG) && !existsSync(paths.PILOT_AWS_CONFIG)) {
-    console.log('AWS configuration detected...copying...')
+    spinner.text = 'Copying AWS configuration'
     copyFile(paths.AWS_CONFIG, paths.PILOT_AWS_CONFIG, err => {
       if (err) {
-        console.error('ERROR: ', err)
-      } else {
-        console.log('AWS config copied!')
+        spinner.fail(failText(`ERROR: ${err}`))
       }
     })
-  } else {
-    console.log('AWS CLI configuration detected')
   }
 
   if (existsSync(paths.AWS_CREDENTIALS)) {
-    console.log('AWS credentials detected...copying...')
+    spinner.text = 'Copying AWS credentials'
     copyFile(paths.AWS_CREDENTIALS, paths.PILOT_AWS_CREDENTIALS, err => {
       if (err) {
-        console.error('ERROR: ', err)
-      } else {
-        console.log('AWS credentials copied!')
+        spinner.fail(failText(`ERROR: ${err}`))
       }
     })
   } else {
-    console.error('ERROR: Please configure AWS CLI with access keys. Run \'aws configure\'')
+    spinner.fail(failText('ERROR: Please configure AWS CLI with access keys. Run \'aws configure\''))
   }
 
   /***********************/
   /*  GCP CONFIGURATION  */
   /***********************/
   if (!existsSync(paths.GCP_CONFIG)) {
-    console.log('No gcloud configuration detected. Ensure you\'ve properly set up your gcloud CLI configuration if deploying with GCP.')
+    spinner.fail(failText('No gcloud configuration detected. Ensure you\'ve properly set up your gcloud CLI configuration if deploying with GCP.'))
     return
   }
   const defaultProject = await gcpCreds.getGCPProject()
 
   if (defaultProject === '') {
-    console.error('ERROR: please set a default project to use via \'gcloud config set project PROJECT_ID\'')
+    spinner.fail(failText('ERROR: please set a default project to use via \'gcloud config set project PROJECT_ID\''))
     return
   }
 
   const defaultZone = await gcpCreds.getGCPZone()
 
   if (defaultZone === '') {
-    console.error('ERROR: please set a default zone to use via \'gcloud config set compute/zone ZONE\'')
+    spinner.fail(failText('ERROR: please set a default zone to use via \'gcloud config set compute/zone ZONE\''))
     return
   }
 
-  console.log('gcloud configuration detected...copying...')
+  spinner.text = 'Copying gcloud configuration'
   copyFile(paths.GCP_CONFIG, paths.PILOT_GCP_CONFIG, err => {
     if (err) {
-      console.error('ERROR: ', err)
-    } else {
-      console.log('gcloud config copy success!')
+      spinner.fail(failText(`ERROR: ${err}`))
     }
   })
 
@@ -108,9 +106,9 @@ export async function initialize(overridePolicyPath: string | null) {
 
   copyFile(templatePath, paths.PILOT_GCP_POLICY, err => {
     if (err) {
-      console.error('ERROR: ', err)
-    } else {
-      console.log('gcloud policy copy success!')
+      spinner.fail(failText(`ERROR: ${err}`))
     }
   })
+
+  spinner.succeed(successText('Files and directories initialized'))
 }
