44f35d67c8fa14d7ed3cef47c7bbf3e07595d6d8
Carl <jerritt.aquino@gmail.com>
2021-08-02 20:05:44 -0400

    update cli output for ui command

--- src/commands/ui.ts
+++ src/commands/ui.ts
@@ -3,6 +3,7 @@ import paths from '../util/paths'
 import { exec } from 'child_process'
 import { readFile } from 'fs'
 import * as open from 'open'
+import { failText } from '../util/cli'
 
 export default class Ui extends Command {
   static description = 'Opens the Waypoint UI on the default browser'
@@ -19,16 +20,28 @@ export default class Ui extends Command {
 
   async run() {
     const { flags } = this.parse(Ui)
+
     if (flags.authenticate) {
       exec(`${paths.WAYPOINT_EXEC} ui -authenticate`, error => {
-        if (error) throw error
+        if (error) {
+          console.log(failText('Unable to open UI. See error below...'))
+          throw error
+        }
       })
     } else {
       readFile(paths.PILOT_AWS_METADATA, 'utf8', (err, data) => {
-        if (err) throw err
+        if (err) {
+          console.log(failText('Unable to open UI. See error below...'))
+          throw err
+        }
 
-        const metadata = JSON.parse(data)
-        open(`https://${metadata.ipAddress}:9702`)
+        // const metadata = JSON.parse(data)
+        const ipAddress = JSON.parse(data).ipAddress
+        if (ipAddress) {
+          open(`https://${ipAddress}:9702`)
+        } else {
+          console.log(failText('No server detected. Run `pilot setup` to initialize a server.'))
+        }
       })
     }
   }
