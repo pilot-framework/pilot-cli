4eb79bca840639e2204738869d5e39f87434e77e
Vincent Bundage <vincentbundage@gmail.com>
2021-08-11 18:31:58 -0600

    feat: invite generates connection command

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,25 +1,47 @@
 import {Command, flags} from '@oclif/command'
+import waypoint from '../util/waypoint'
+import fsUtil from '../util/fs'
+import awsExec from '../util/aws/exec'
+import gcpExec from '../util/gcp/exec'
+import { pilotText, failText, grayText } from '../util/cli'
 
 export default class Invite extends Command {
-  static description = 'describe the command here'
+  static description = 'Creates an invite string that can be sent to team members'
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(Invite)
+    this.parse(Invite)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/vbundage/capstone/cli-dev/pilot-cli/src/commands/invite.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const serverPlatform = (await fsUtil.getPilotMetadata()).serverPlatform
+    if (!serverPlatform) {
+      this.log(failText('No server found'))
+      return
     }
+
+    let ipAddr = ''
+
+    if (serverPlatform === 'aws') {
+      try {
+        ipAddr = await awsExec.getServerIP()
+      } catch {
+        this.log(failText('No server found'))
+        return
+      }
+    }
+    if (serverPlatform === 'gcp') {
+      try {
+        ipAddr = await gcpExec.getServerIP()
+      } catch {
+        this.log(failText('No server found'))
+        return
+      }
+    }
+
+    const token = await waypoint.getToken()
+    this.log(`${grayText('Provide the following command to your team member')}
+${pilotText(`pilot connect --address=${ipAddr} --token=${token}`)}`)
   }
 }
